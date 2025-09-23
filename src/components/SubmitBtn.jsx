01a74389b415de7e1e978ff6acff21955237433e
Muhammad Shahid Khan <shahidlanger0@gmail.com>
2025-09-23 21:22:04 +0500

    created login page and the UI

---
+++ src/components/SubmitBtn.jsx
@@ -0,0 +1,9 @@
+const SubmitBtn = ({ text }) => {
+  return (
+    <button type="submit" className="btn btn-primary uppercase">
+      {text}
+    </button>
+  );
+};
+
+export default SubmitBtn;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,41 @@
+import { FormInput, SubmitBtn } from "../components";
+import { Link, Form } from "react-router-dom";
+
 const Login = () => {
   return (
-    <div>
-      <h2>Login</h2>
-    </div>
+    <section className="grid min-h-screen place-items-center">
+      <Form
+        method="POST"
+        className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4">
+        <h4 className="text-center text-3xl font-bold">login</h4>
+        <FormInput
+          type="email"
+          label="email"
+          name="identifier"
+          defaultValue="test@test.com"
+        />
+        <FormInput
+          type="password"
+          label="password"
+          aname="password"
+          defaultValue="secret"
+        />
+        <div className="mt-4">
+          <SubmitBtn text="login" />
+        </div>
+        <button type="button" className="btn btn-secondary btn-block uppercase">
+          guest user
+        </button>
+        <p className="text-center">
+          Not a member yet?{" "}
+          <Link
+            to="register"
+            className="ml-2 link link-hover link-primary capitalize">
+            register
+          </Link>
+        </p>
+      </Form>
+    </section>
   );
 };
 
