8332b5cebfcdc6bf60d83cfadd5bed6ae5af8e50
Muhammad Shahid Khan <shahidlanger0@gmail.com>
2025-09-27 22:06:44 +0500

    updated filters component and created some more components, like forminput, select input, and two buttons

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,8 +1,48 @@
+import { Form, useLoaderData, Link } from "react-router-dom";
+import FormInput from "./FormInput";
+import SelectInput from "./SelectInput";
+
 const Filters = () => {
+  const { meta } = useLoaderData();
+
   return (
-    <div>
-      <h2>Filters</h2>
-    </div>
+    <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
+      {/* SEARCH */}
+      <FormInput
+        type="search"
+        name="search"
+        label="Search product"
+        size="input-sm"
+      />
+      {/* CATEGORIES */}
+      <SelectInput
+        label="select category"
+        name="category"
+        list={meta.categories}
+        size="select-sm"
+      />
+      {/* COMPANIES */}
+      <SelectInput
+        label="select company"
+        name="company"
+        list={meta.companies}
+        size="select-sm"
+      />
+      {/* ORDER */}
+      <SelectInput
+        label="sort by"
+        name="order"
+        list={["a-z", "A-Z", "hight", "low"]}
+        size="select-sm"
+      />
+      {/* BUTTONS */}
+      <button className="btn btn-primary btn-sm capitalize" type="button">
+        search
+      </button>
+      <Link to="/products" className="btn btn-accent btn-sm capitalize">
+        reset
+      </Link>
+    </Form>
   );
 };
 
---
+++ src/components/SelectInput.jsx
@@ -0,0 +1,24 @@
+const SelectInput = ({ label, name, list, defaultValue, size }) => {
+  return (
+    <div className="form-control">
+      <label htmlFor={name} className="label">
+        <span className="label-text capitalize mb-1 ">{label}</span>
+      </label>
+      <select
+        name={name}
+        className={`select select-bordered ${size}`}
+        defaultValue={defaultValue}
+        id={name}>
+        {list.map((item) => {
+          return (
+            <option key={item} value={item} className="">
+              {item}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default SelectInput;
