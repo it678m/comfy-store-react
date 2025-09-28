138255cc399a92e314ac66f4d9127bf50aa99d49
Muhammad Shahid Khan <shahidlanger0@gmail.com>
2025-09-28 10:08:52 +0500

    updated and setup the pagination component

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -1,7 +1,45 @@
+import { useLoaderData, useLocation, useNavigation } from "react-router-dom";
+
 const PaginationContainer = () => {
+  const { meta } = useLoaderData();
+  const { pageCount, page } = meta.pagination;
+
+  const pages = Array.from({ length: pageCount }, (_, index) => {
+    return index + 1;
+  });
+
+  const handlePageChange = (pageNumber) => {
+    console.log(pageNumber);
+  };
+
+  if (pageCount < 2) return null;
+
   return (
-    <div>
-      <h2>PaginationContainer</h2>
+    <div className="mt-16 flex justify-end">
+      <div className="join">
+        <button
+          className="join-item btn btn-xs sm:btn-md"
+          onClick={() => handlePageChange("prev")}>
+          Prev
+        </button>
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              key={pageNumber}
+              onClick={() => handlePageChange(pageNumber)}
+              className={`btn btn-xs sm:btn-md border-none join-item ${
+                pageNumber === page ? "bg-base-300 border-base-300" : ""
+              }`}>
+              {pageNumber}
+            </button>
+          );
+        })}
+        <button
+          className="join-item btn btn-xs sm:btn-md"
+          onClick={() => handlePageChange("next")}>
+          Next
+        </button>
+      </div>
     </div>
   );
 };
