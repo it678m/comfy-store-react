e7eb934bffea52c154374e5d342e41fe928a660f
Muhammad Shahid Khan <shahidlanger0@gmail.com>
2025-09-29 11:29:07 +0500

    fixed another bug in carttotal, now complete and working

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -63,7 +63,7 @@ const cartSlice = createSlice({
     },
     calculateTotals: (state) => {
       state.tax = 0.1 * state.cartTotal;
-      state.orderTotal += state.cartTotal + state.shipping + state.tax;
+      state.orderTotal = state.cartTotal + state.shipping + state.tax;
       localStorage.setItem("cart", JSON.stringify(state));
     },
   },
